af15135604c159ff9d06602859934cf13df5ab42
ninkadp <ninadepalma@gmail.com>
2023-08-11 10:18:04 -0400

    initial commit

---
+++ early-init.el
@@ -0,0 +1,9 @@
+;; https://github.com/radian-software/straight.el#getting-started
+;; Users of Emacs versions >= 27 will want to add:
+
+(setq package-enable-at-startup nil)
+
+;; to their early init-file to prevent package.el loading packages prior to their init-file loading.
+
+(provide 'early-init)
+
