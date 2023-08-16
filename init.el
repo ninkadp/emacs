a650d98d7c65b92c8499627c23e1743bec7445ec
ninkadp <ninadepalma@gmail.com>
2023-08-16 11:33:14 -0400

    move contents of .emacs to init.el

--- init.el
+++ init.el
@@ -1,1 +1,17 @@
-(setq python-shell-interpreter "python3")
+(package-initialize)
+(org-babel-load-file "~/.emacs.d/nina.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("937dff6e81f28a21e72f8d6eb204ddd69fb8d819cd6dd7ed347b79b94e392453" "dd37ed483cad192523a0014de08ad7bc93e6ffd454e4fa66b2f856141d88a4e7" default))
+ '(org-agenda-files '("~/org/work/dash.org"))
+ '(package-selected-packages '(org-books go-translate elpy use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
